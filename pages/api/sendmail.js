7b04526ac5cbae115c405a4617f3d50282465c72
Mark <mark2kk@gmail.com>
2022-01-10 12:21:18 +0100

    Updated email function

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -19,38 +17,8 @@ export default async (req, res) => {
   sgMail
     .send(mailBody)
     .then((response) => {
+      console.log("response", response);
       res.status(200).json({ message: "Message Sent" });
     })
     .catch((err) => {});
-
-  res.status(200).json({ name: "John Doe" });
 };
-
-// function getMessage() {
-//   const body = 'This is a test email using SendGrid from Node.js';
-//   return {
-//     to: 'you@domain.com',
-//     from: 'verifiedemail@previousstep.com',
-//     subject: 'Test email with Node.js and SendGrid',
-//     text: body,
-//     html: `<strong>${body}</strong>`,
-//   };
-// }
-
-// async function sendEmail() {
-//   try {
-//     await sendGridMail.send(getMessage());
-//     console.log('Test email sent successfully');
-//   } catch (error) {
-//     console.error('Error sending test email');
-//     console.error(error);
-//     if (error.response) {
-//       console.error(error.response.body)
-//     }
-//   }
-// }
-
-// (async () => {
-//   console.log('Sending test email');
-//   await sendEmail();
-// })();
